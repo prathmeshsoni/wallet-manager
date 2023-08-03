929c6406fd7da3d86d87d022d7d440c2489a9a5b
prathmesh soni <prathmesh.soni@gmail.com>
2023-08-03 22:44:35 +0530

    update test

--- wallet/passenger_wsgi.py
+++ wallet/passenger_wsgi.py
@@ -2,12 +2,14 @@ import os
 import sys
 
 
-sys.path.insert(0, os.path.dirname(__file__))
+from wallet.wsgi import application
+
+# sys.path.insert(0, os.path.dirname(__file__))
 
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python %s\n' % sys.version.split()[0]
-    response = '\n'.join([message, version])
-    return [response.encode()]
+# def application(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     message = 'It works!\n'
+#     version = 'Python %s\n' % sys.version.split()[0]
+#     response = '\n'.join([message, version])
+#     return [response.encode()]
