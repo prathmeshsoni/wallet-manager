26e96199c33a0365e292244ebfef205c21cbe280
prathmesh soni <prathmesh.soni@gmail.com>
2023-08-03 22:04:47 +0530

    update

--- wallet/man.py
+++
@@ -1,7 +0,0 @@
-from django.core.management import call_command
-from django.core.management.commands.migrate import Command as MigrateCommand
-
-class Command(MigrateCommand):
-    def handle(self, *args, **options):
-        options['exclude'] = ['category']
-        super().handle(*args, **options)
--- wallet/passenger_wsgi.py
+++ wallet/passenger_wsgi.py
@@ -4,11 +4,10 @@ import sys
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-from wallet.wsgi import application
 
-# def application(environ, start_response):
-#     start_response('200 OK', [('Content-Type', 'text/plain')])
-#     message = 'It works!\n'
-#     version = 'Python %s\n' % sys.version.split()[0]
-#     response = '\n'.join([message, version])
-#     return [response.encode()]
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'It works!\n'
+    version = 'Python %s\n' % sys.version.split()[0]
+    response = '\n'.join([message, version])
+    return [response.encode()]
